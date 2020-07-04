673fb996524483b8e3fe78db3426140dd31eaaba
Aseem Srivastava <aseemsrivastava9@gmail.com>
2020-07-04 17:03:26 +0530

    Added implementaion of snake and ladder
    Completed the code

--- Snake&Ladder.py
+++ Snake&Ladder.py
@@ -22,9 +22,9 @@ board = [	100, 99, 98, 97, 96, 95, 94, 93, 92, 91,
 			1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 		]
 	
-dice = (1, 2, 3, 4, 5, 6)
-player1 = input("Enter name of player1 ")
-player2 = input("Enter name of player2 ")
+dice = ( 1, 2, 3, 4, 5, 6 )
+player1 = input( "Enter name of player1 " )
+player2 = input( "Enter name of player2 " )
 
 
 
@@ -43,8 +43,10 @@ while(count != 5):
 				snake.add(start, end)
 				count = count + 1
 
-print(snake)
+print( "These are the starting position of snakes generated and where they will end" )
+print( snake )
 time.sleep(5)
+print()
 
 ladder = my_dictionary()
 
@@ -62,27 +64,50 @@ while(count != 5):
 		
 
 
-print(ladder)
+print( "These are the starting position of ladders generated and where they will end" )
+print( ladder )
 time.sleep(5)
+print()
 count = random.randint(1, 2)
+
+print( " Note:- Comment line 76 and 96 if you dont want to press Enter for every turn / Or want to finish execution of program in one go")
+print()
+time.sleep(2)
 
 while player1Pos != 100 and player2Pos != 100:
 	x = int(random.choice(dice))
 	if count == 1:
+		input( player1+ " Press Enter to roll the dice" ) #Comment this line if you dont want to press enter again and again for Player1
+		print( "Number occurred on dice "+str(x) )
 		if player1Pos + x <= 100:
 			player1Pos = player1Pos + x
-			print( str(player1)+ " moves by " +str(x)+ " blocks")
+			print( str(player1)+ " moves by " +str(x)+ " blocks" )
+			if snake.get(player1Pos) != None:
+				print( "Oh no!!! "+player1+ " found a snake!!! at position "+str(player1Pos)+" ....Moving down" )
+				player1Pos = snake.get(player1Pos)
+			elif ladder.get(player1Pos) != None:
+				print( "Hurray!!! "+player1+ " found a ladder!!! at position "+str(player1Pos)+" ...Moving up" )
+				player1Pos = ladder.get(player1Pos)
 		else:
-			print("Position of " +player1+ " exceed 100... therefore not moving")
+			print( "Position of " +player1+ " exceed 100... therefore not moving" )
 		count = 2
-		print("Current Position of " +str(player1)+ " is " +str(player1Pos) )
-		print("Current Position of " +str(player2)+ " is " +str(player2Pos) )
+		
+		print( "Current Position of " +str(player1)+ " is " +str(player1Pos) )
+		print( "Current Position of " +str(player2)+ " is " +str(player2Pos) )
 		print()
 
 	elif count == 2:
+		input( player2+ " Press Enter to roll the dice" ) #Comment this line if you dont want to press enter again and again for Player2
+		print( "Number occurred on dice "+str(x) )
 		if player2Pos + x <= 100:
 			player2Pos = player2Pos + x
 			print( str(player2)+ " moves by " + str(x)+ " blocks")
+			if snake.get(player2Pos) != None:
+				print("Oh no!!! "+player2+ " found a snake!!! at position "+str(player2Pos)+" ....Moving down")
+				player2Pos = snake.get(player2Pos)
+			elif ladder.get(player2Pos) != None:
+				print("Hurray!!! "+player2+ " found a ladder!!! at position "+str(player2Pos)+" ...Moving up")
+				player2Pos = ladder.get(player2Pos)
 		else:
 			print("Position of " +player2+ " exceed 100... therefore not moving")
 		count = 1
